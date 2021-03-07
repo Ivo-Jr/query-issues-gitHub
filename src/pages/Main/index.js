af2e7a32efa39e7a91ba32c855aded2156e87aed
IvoJr <maciel.jr95@gmail.com>
2021-03-07 16:38:56 -0300

    initial commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
